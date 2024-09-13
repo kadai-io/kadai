011aeebb8aa574c27014f686d53da4fb25308472
CRoberto1926 <3205022+CRoberto1926@users.noreply.github.com>
2024-09-13 12:14:45 +0200

    Update copyright and license header for all files

--- routing/dmn-xlsx-converter/src/test/java/org/camunda/bpm/dmn/xlsx/TestHelper.java
+++ routing/dmn-xlsx-converter/src/test/java/org/camunda/bpm/dmn/xlsx/TestHelper.java
@@ -1,15 +1,22 @@
-/* Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+/*
+ * Copyright [2024] [envite consulting GmbH]
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ *
  */
+
+
 package org.camunda.bpm.dmn.xlsx;
 
 import static org.assertj.core.api.Assertions.assertThat;
