fa024cd436ad2352729d5c2adfa54a42858932f1
ryzheboka <25465835+ryzheboka@users.noreply.github.com>
2024-12-20 11:20:05 +0100

    Closes #341 - manually update poms to version 9.2.1-SNAPSHOT

--- rest/kadai-rest-spring-example-wildfly/src/test/java/io/kadai/example/wildfly/AbstractAccTest.java
+++ rest/kadai-rest-spring-example-wildfly/src/test/java/io/kadai/example/wildfly/AbstractAccTest.java
@@ -41,7 +41,7 @@ import org.springframework.http.ResponseEntity;
 @KadaiSpringBootTest
 public class AbstractAccTest {
 
-  protected static final String DEPENDENCY_VERSION = "9.1.1-SNAPSHOT";
+  protected static final String DEPENDENCY_VERSION = "9.2.1-SNAPSHOT";
 
   private static final Logger LOGGER = LoggerFactory.getLogger(AbstractAccTest.class);
 
