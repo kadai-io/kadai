5afefb74877f5b3fa5ae845dfabc5fdff41433cb
Julian Bruder <julian.bruder@outlook.com>
2025-01-22 14:13:44 +0100

    KAD-459: specify `UserMapper::findByOrgLevel1/2/3/4`

--- lib/kadai-core/src/main/java/io/kadai/user/internal/UserMapper.java
+++ lib/kadai-core/src/main/java/io/kadai/user/internal/UserMapper.java
@@ -73,6 +73,90 @@ public interface UserMapper {
   @SelectProvider(type = UserMapperSqlProvider.class, method = "findByIds")
   List<UserImpl> findByIds(@Param("ids") Set<String> ids);
 
+  @Result(property = "id", column = "USER_ID")
+  @Result(property = "groups", column = "USER_ID", many = @Many(select = "findGroupsById"))
+  @Result(
+      property = "permissions",
+      column = "USER_ID",
+      many = @Many(select = "findPermissionsById"))
+  @Result(property = "firstName", column = "FIRST_NAME")
+  @Result(property = "lastName", column = "LASTNAME")
+  @Result(property = "fullName", column = "FULL_NAME")
+  @Result(property = "longName", column = "LONG_NAME")
+  @Result(property = "email", column = "E_MAIL")
+  @Result(property = "phone", column = "PHONE")
+  @Result(property = "mobilePhone", column = "MOBILE_PHONE")
+  @Result(property = "orgLevel4", column = "ORG_LEVEL_4")
+  @Result(property = "orgLevel3", column = "ORG_LEVEL_3")
+  @Result(property = "orgLevel2", column = "ORG_LEVEL_2")
+  @Result(property = "orgLevel1", column = "ORG_LEVEL_1")
+  @Result(property = "data", column = "DATA")
+  @SelectProvider(type = UserMapperSqlProvider.class, method = "findByOrgLevel1")
+  List<UserImpl> findByOrgLevel1(@Param("orgLevel") String orgLevel);
+
+  @Result(property = "id", column = "USER_ID")
+  @Result(property = "groups", column = "USER_ID", many = @Many(select = "findGroupsById"))
+  @Result(
+      property = "permissions",
+      column = "USER_ID",
+      many = @Many(select = "findPermissionsById"))
+  @Result(property = "firstName", column = "FIRST_NAME")
+  @Result(property = "lastName", column = "LASTNAME")
+  @Result(property = "fullName", column = "FULL_NAME")
+  @Result(property = "longName", column = "LONG_NAME")
+  @Result(property = "email", column = "E_MAIL")
+  @Result(property = "phone", column = "PHONE")
+  @Result(property = "mobilePhone", column = "MOBILE_PHONE")
+  @Result(property = "orgLevel4", column = "ORG_LEVEL_4")
+  @Result(property = "orgLevel3", column = "ORG_LEVEL_3")
+  @Result(property = "orgLevel2", column = "ORG_LEVEL_2")
+  @Result(property = "orgLevel1", column = "ORG_LEVEL_1")
+  @Result(property = "data", column = "DATA")
+  @SelectProvider(type = UserMapperSqlProvider.class, method = "findByOrgLevel2")
+  List<UserImpl> findByOrgLevel2(@Param("orgLevel") String orgLevel);
+
+  @Result(property = "id", column = "USER_ID")
+  @Result(property = "groups", column = "USER_ID", many = @Many(select = "findGroupsById"))
+  @Result(
+      property = "permissions",
+      column = "USER_ID",
+      many = @Many(select = "findPermissionsById"))
+  @Result(property = "firstName", column = "FIRST_NAME")
+  @Result(property = "lastName", column = "LASTNAME")
+  @Result(property = "fullName", column = "FULL_NAME")
+  @Result(property = "longName", column = "LONG_NAME")
+  @Result(property = "email", column = "E_MAIL")
+  @Result(property = "phone", column = "PHONE")
+  @Result(property = "mobilePhone", column = "MOBILE_PHONE")
+  @Result(property = "orgLevel4", column = "ORG_LEVEL_4")
+  @Result(property = "orgLevel3", column = "ORG_LEVEL_3")
+  @Result(property = "orgLevel2", column = "ORG_LEVEL_2")
+  @Result(property = "orgLevel1", column = "ORG_LEVEL_1")
+  @Result(property = "data", column = "DATA")
+  @SelectProvider(type = UserMapperSqlProvider.class, method = "findByOrgLevel3")
+  List<UserImpl> findByOrgLevel3(@Param("orgLevel") String orgLevel);
+
+  @Result(property = "id", column = "USER_ID")
+  @Result(property = "groups", column = "USER_ID", many = @Many(select = "findGroupsById"))
+  @Result(
+      property = "permissions",
+      column = "USER_ID",
+      many = @Many(select = "findPermissionsById"))
+  @Result(property = "firstName", column = "FIRST_NAME")
+  @Result(property = "lastName", column = "LASTNAME")
+  @Result(property = "fullName", column = "FULL_NAME")
+  @Result(property = "longName", column = "LONG_NAME")
+  @Result(property = "email", column = "E_MAIL")
+  @Result(property = "phone", column = "PHONE")
+  @Result(property = "mobilePhone", column = "MOBILE_PHONE")
+  @Result(property = "orgLevel4", column = "ORG_LEVEL_4")
+  @Result(property = "orgLevel3", column = "ORG_LEVEL_3")
+  @Result(property = "orgLevel2", column = "ORG_LEVEL_2")
+  @Result(property = "orgLevel1", column = "ORG_LEVEL_1")
+  @Result(property = "data", column = "DATA")
+  @SelectProvider(type = UserMapperSqlProvider.class, method = "findByOrgLevel4")
+  List<UserImpl> findByOrgLevel4(@Param("orgLevel") String orgLevel);
+
   @SelectProvider(type = UserMapperSqlProvider.class, method = "findGroupsById")
   Set<String> findGroupsById(String id);
 
