b462b077b1111ba103e7139fa1f67ebb5f3440d1
Julian Bruder <julian.bruder@outlook.com>
2025-01-24 17:10:43 +0100

    KAD-83: add `LambdaExceptionUtil`

---
+++ rest/kadai-rest-spring/src/main/java/io/kadai/common/rest/util/LambdaExceptionUtil.java
@@ -0,0 +1,44 @@
+package io.kadai.common.rest.util;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public final class LambdaExceptionUtil {
+
+  @FunctionalInterface
+  public interface Consumer_WithExceptions<T, E extends Exception> {
+    void accept(T t) throws E;
+  }
+
+  @FunctionalInterface
+  public interface Function_WithExceptions<T, R, E extends Exception> {
+    R apply(T t) throws E;
+  }
+
+  public static <T, E extends Exception> Consumer<T> rethrowConsumer(Consumer_WithExceptions<T, E> consumer) throws E {
+    return t -> {
+      try {
+        consumer.accept(t);
+      } catch (Exception exception) {
+        throwActualException(exception);
+      }
+    };
+  }
+
+  public static <T, R, E extends Exception> Function<T, R> rethrowFunction(Function_WithExceptions<T, R, E> function) throws E  {
+    return t -> {
+      try {
+        return function.apply(t);
+      } catch (Exception exception) {
+        throwActualException(exception);
+        return null;
+      }
+    };
+  }
+
+  @SuppressWarnings("unchecked")
+  private static <E extends Exception> void throwActualException(Exception exception) throws E {
+    throw (E) exception;
+  }
+
+}
