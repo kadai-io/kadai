e945e1cd09ede02d5e20953531fa14f37791e70e
Julian Bruder <julian.bruder@outlook.com>
2025-01-28 13:16:59 +0100

    KAD-83: fix check-style

--- rest/kadai-rest-spring/src/main/java/io/kadai/common/rest/util/LambdaExceptionUtil.java
+++ rest/kadai-rest-spring/src/main/java/io/kadai/common/rest/util/LambdaExceptionUtil.java
@@ -6,16 +6,17 @@ import java.util.function.Function;
 public final class LambdaExceptionUtil {
 
   @FunctionalInterface
-  public interface Consumer_WithExceptions<T, E extends Exception> {
+  public interface ConsumerWithExceptions<T, E extends Exception> {
     void accept(T t) throws E;
   }
 
   @FunctionalInterface
-  public interface Function_WithExceptions<T, R, E extends Exception> {
+  public interface FunctionWithExceptions<T, R, E extends Exception> {
     R apply(T t) throws E;
   }
 
-  public static <T, E extends Exception> Consumer<T> rethrowConsumer(Consumer_WithExceptions<T, E> consumer) throws E {
+  public static <T, E extends Exception> Consumer<T> rethrowConsumer(
+      ConsumerWithExceptions<T, E> consumer) throws E {
     return t -> {
       try {
         consumer.accept(t);
@@ -25,7 +26,8 @@ public final class LambdaExceptionUtil {
     };
   }
 
-  public static <T, R, E extends Exception> Function<T, R> rethrowFunction(Function_WithExceptions<T, R, E> function) throws E  {
+  public static <T, R, E extends Exception> Function<T, R> rethrowFunction(
+      FunctionWithExceptions<T, R, E> function) throws E  {
     return t -> {
       try {
         return function.apply(t);
