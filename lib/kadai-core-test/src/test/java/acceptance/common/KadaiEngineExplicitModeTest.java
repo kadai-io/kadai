6633803310c9e32c63e56253f9312c261176f5d3
Julian Bruder <julian.bruder@outlook.com>
2025-02-05 19:38:48 +0100

    KAD-512: test `EXPLICIT`

--- lib/kadai-core-test/src/test/java/acceptance/common/KadaiEngineExplicitModeTest.java
+++ lib/kadai-core-test/src/test/java/acceptance/common/KadaiEngineExplicitModeTest.java
@@ -1,6 +1,7 @@
 package acceptance.common;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import io.kadai.KadaiConfiguration;
 import io.kadai.common.api.KadaiEngine;
@@ -8,15 +9,20 @@ import io.kadai.common.api.KadaiEngine.ConnectionManagementMode;
 import io.kadai.testapi.KadaiIntegrationTest;
 import io.kadai.testapi.builder.UserBuilder;
 import io.kadai.testapi.extensions.TestContainerExtension;
+import io.kadai.user.api.exceptions.UserNotFoundException;
 import io.kadai.user.api.models.User;
+import java.sql.Connection;
 import javax.sql.DataSource;
+import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 @KadaiIntegrationTest
-public class KadaiEngineAutocommitModeTest {
+public class KadaiEngineExplicitModeTest {
 
   private KadaiEngine thisKadaiEngine;
+  private Connection thisConnection;
   private KadaiEngine thatKadaiEngine;
 
   @BeforeEach
@@ -28,12 +34,20 @@ public class KadaiEngineAutocommitModeTest {
             .initKadaiProperties()
             .build();
     this.thisKadaiEngine =
-        KadaiEngine.buildKadaiEngine(kadaiConfiguration, ConnectionManagementMode.AUTOCOMMIT);
+        KadaiEngine.buildKadaiEngine(kadaiConfiguration, ConnectionManagementMode.EXPLICIT);
+    this.thisConnection = dataSource.getConnection();
+    thisKadaiEngine.setConnection(this.thisConnection);
+
     this.thatKadaiEngine = KadaiEngine.buildKadaiEngine(kadaiConfiguration);
   }
 
+  @AfterEach
+  void cleanConnections() {
+    thisKadaiEngine.closeConnection();
+  }
+
   @Test
-  void should_RetrieveCreated() throws Exception {
+  void should_RetrieveCreated_When_Committed() throws Exception {
     User expected =
         UserBuilder.newUser()
             .id("user-1-1")
@@ -41,9 +55,25 @@ public class KadaiEngineAutocommitModeTest {
             .lastName("Mustermann")
             .longName("Long name of user-1-1")
             .buildAndStore(thisKadaiEngine.getUserService());
+    thisConnection.commit();
 
     User actual = thatKadaiEngine.getUserService().getUser(expected.getId());
 
     assertThat(actual).isEqualTo(expected);
+  }
+
+  @Test
+  void should_NotRetrieveCreated_When_NotCommitted() throws Exception {
+    User user =
+        UserBuilder.newUser()
+            .id("user-1-1")
+            .firstName("Max")
+            .lastName("Mustermann")
+            .longName("Long name of user-1-1")
+            .buildAndStore(thisKadaiEngine.getUserService());
+
+    ThrowingCallable call = () -> thatKadaiEngine.getUserService().getUser(user.getId());
+
+    assertThatExceptionOfType(UserNotFoundException.class).isThrownBy(call);
   }
 }
