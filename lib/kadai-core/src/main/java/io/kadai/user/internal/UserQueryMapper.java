eae1b42ba09347b9e10b52b237e921fca15ccbd4
Julian Bruder <julian.bruder@outlook.com>
2025-02-07 13:42:02 +0100

    KAD-459: add `UserQuerySqlProvider`

--- lib/kadai-core/src/main/java/io/kadai/user/internal/UserQueryMapper.java
+++ lib/kadai-core/src/main/java/io/kadai/user/internal/UserQueryMapper.java
@@ -4,33 +4,14 @@ import io.kadai.user.internal.models.UserImpl;
 import java.util.List;
 import org.apache.ibatis.annotations.Many;
 import org.apache.ibatis.annotations.Result;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.SelectProvider;
 
 /**
  * This interface provides a Mapper for querying {@linkplain io.kadai.user.api.models.User Users}.
  */
 public interface UserQueryMapper {
 
-  @Select(
-      "<script>SELECT USER_ID, FIRST_NAME, LASTNAME, FULL_NAME, LONG_NAME, E_MAIL, PHONE, "
-          + "MOBILE_PHONE, ORG_LEVEL_4, ORG_LEVEL_3, ORG_LEVEL_2, ORG_LEVEL_1, DATA "
-          + "FROM USER_INFO "
-          + "<where>"
-          + "<if test='idIn != null'>AND USER_ID IN"
-          + "(<foreach item='item' collection='idIn' separator=',' >#{item}</foreach>)</if> "
-          + "<if test='orgLevel1In != null'>AND ORG_LEVEL_1 IN"
-          + "(<foreach item='item' collection='orgLevel1In' separator=',' >#{item}</foreach>)</if> "
-          + "<if test='orgLevel2In != null'>AND ORG_LEVEL_2 IN"
-          + "(<foreach item='item' collection='orgLevel2In' separator=',' >#{item}</foreach>)</if> "
-          + "<if test='orgLevel3In != null'>AND ORG_LEVEL_3 IN"
-          + "(<foreach item='item' collection='orgLevel3In' separator=',' >#{item}</foreach>)</if> "
-          + "<if test='orgLevel4In != null'>AND ORG_LEVEL_4 IN"
-          + "(<foreach item='item' collection='orgLevel4In' separator=',' >#{item}</foreach>)</if> "
-          + "</where>"
-          + "<if test='!orderBy.isEmpty()'>ORDER BY "
-          + "<foreach item='item' collection='orderBy' separator=',' >${item}</foreach></if> "
-          + "<if test=\"_databaseId == 'db2'\">with UR </if> "
-          + "</script>")
+  @SelectProvider(type = UserQuerySqlProvider.class, method = "queryUsers")
   @Result(property = "id", column = "USER_ID")
   @Result(
       property = "groups",
@@ -54,43 +35,9 @@ public interface UserQueryMapper {
   @Result(property = "data", column = "DATA")
   List<UserImpl> queryUsers(UserQueryImpl userQuery);
 
-  @Select(
-      "<script>SELECT COUNT(USER_ID) FROM USER_INFO "
-          + "<where>"
-          + "<if test='idIn != null'>AND USER_ID IN"
-          + "(<foreach item='item' collection='idIn' separator=',' >#{item}</foreach>)</if> "
-          + "<if test='orgLevel1In != null'>AND ORG_LEVEL_1 IN"
-          + "(<foreach item='item' collection='orgLevel1In' separator=',' >#{item}</foreach>)</if> "
-          + "<if test='orgLevel2In != null'>AND ORG_LEVEL_2 IN"
-          + "(<foreach item='item' collection='orgLevel2In' separator=',' >#{item}</foreach>)</if> "
-          + "<if test='orgLevel3In != null'>AND ORG_LEVEL_3 IN"
-          + "(<foreach item='item' collection='orgLevel3In' separator=',' >#{item}</foreach>)</if> "
-          + "<if test='orgLevel4In != null'>AND ORG_LEVEL_4 IN"
-          + "(<foreach item='item' collection='orgLevel4In' separator=',' >#{item}</foreach>)</if> "
-          + "</where>"
-          + "<if test='!orderBy.isEmpty()'>ORDER BY "
-          + "<foreach item='item' collection='orderBy' separator=',' >${item}</foreach></if> "
-          + "<if test=\"_databaseId == 'db2'\">with UR </if> "
-          + "</script>")
-  List<UserImpl> countQueryUsers(UserQueryImpl userQuery);
+  @SelectProvider(type = UserQuerySqlProvider.class, method = "countQueryUsers")
+  Long countQueryUsers(UserQueryImpl userQuery);
 
-  @Select(
-      "<script>SELECT DISTINCT ${columnName} FROM USER_INFO "
-          + "<where>"
-          + "<if test='idIn != null'>AND USER_ID IN"
-          + "(<foreach item='item' collection='idIn' separator=',' >#{item}</foreach>)</if> "
-          + "<if test='orgLevel1In != null'>AND ORG_LEVEL_1 IN"
-          + "(<foreach item='item' collection='orgLevel1In' separator=',' >#{item}</foreach>)</if> "
-          + "<if test='orgLevel2In != null'>AND ORG_LEVEL_2 IN"
-          + "(<foreach item='item' collection='orgLevel2In' separator=',' >#{item}</foreach>)</if> "
-          + "<if test='orgLevel3In != null'>AND ORG_LEVEL_3 IN"
-          + "(<foreach item='item' collection='orgLevel3In' separator=',' >#{item}</foreach>)</if> "
-          + "<if test='orgLevel4In != null'>AND ORG_LEVEL_4 IN"
-          + "(<foreach item='item' collection='orgLevel4In' separator=',' >#{item}</foreach>)</if> "
-          + "</where>"
-          + "<if test='!orderBy.isEmpty()'>ORDER BY "
-          + "<foreach item='item' collection='orderBy' separator=',' >${item}</foreach></if> "
-          + "<if test=\"_databaseId == 'db2'\">with UR </if> "
-          + "</script>")
+  @SelectProvider(type = UserQuerySqlProvider.class, method = "queryUserColumnValues")
   List<String> queryUserColumnValues(UserQueryImpl userQuery);
 }
