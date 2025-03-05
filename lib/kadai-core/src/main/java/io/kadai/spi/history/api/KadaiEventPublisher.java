79100eb6f3cca151c0f8876ecb4810dbe9e6d361
Julian Bruder <julian.bruder@outlook.com>
2025-03-05 15:14:00 +0100

    KAD-545: add broader default methods to publish events

--- lib/kadai-core/src/main/java/io/kadai/spi/history/api/KadaiEventPublisher.java
+++ lib/kadai-core/src/main/java/io/kadai/spi/history/api/KadaiEventPublisher.java
@@ -1,8 +1,27 @@
 package io.kadai.spi.history.api;
 
 import io.kadai.spi.history.api.events.KadaiEvent;
+import java.util.Collection;
+import java.util.Optional;
+import java.util.function.Supplier;
 
 public interface KadaiEventPublisher<T extends KadaiEvent> {
 
   void publish(T event);
+
+  default void publishing(Supplier<T> supplyEvent) {
+    publish(supplyEvent.get());
+  }
+
+  default void tryPublishing(Supplier<Optional<T>> trySupplyEvent) {
+    trySupplyEvent.get().ifPresent(this::publish);
+  }
+
+  default void publishAll(Collection<T> events) {
+    events.forEach(this::publish);
+  }
+
+  default void publishingAll(Supplier<Collection<T>> supplyEvents) {
+    supplyEvents.get().forEach(this::publish);
+  }
 }
--- lib/kadai-core/src/main/java/io/kadai/spi/history/internal/SimpleKadaiEventPublisherImpl.java
+++ lib/kadai-core/src/main/java/io/kadai/spi/history/internal/SimpleKadaiEventPublisherImpl.java
@@ -2,6 +2,9 @@ package io.kadai.spi.history.internal;
 
 import io.kadai.spi.history.api.KadaiEventPublisher;
 import io.kadai.spi.history.api.events.KadaiEvent;
+import java.util.Collection;
+import java.util.Optional;
+import java.util.function.Supplier;
 
 public class SimpleKadaiEventPublisherImpl<T extends KadaiEvent> implements KadaiEventPublisher<T> {
 
@@ -14,5 +17,26 @@ public class SimpleKadaiEventPublisherImpl<T extends KadaiEvent> implements Kada
   @Override
   public void publish(T event) {
     historyEventManager.forward(event);
+  }
+
+  @Override
+  public void publishing(Supplier<T> supplyEvent) {
+    if (historyEventManager.isEnabled()) {
+      historyEventManager.forward(supplyEvent.get());
+    }
+  }
+
+  @Override
+  public void tryPublishing(Supplier<Optional<T>> trySupplyEvent) {
+    if (historyEventManager.isEnabled()) {
+      trySupplyEvent.get().ifPresent(this::publish);
+    }
+  }
+
+  @Override
+  public void publishingAll(Supplier<Collection<T>> supplyEvents) {
+    if (historyEventManager.isEnabled()) {
+      supplyEvents.get().forEach(this::publish);
+    }
   }
 }
