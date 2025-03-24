3c22cf0848256d02e4f81bc9a45b34f22a98dc5c
Julian Bruder <julian.bruder@outlook.com>
2025-03-24 22:30:08 +0100

    KAD-606: add `PageInterceptor`

--- common/kadai-common/src/main/java/io/kadai/common/internal/configuration/DB.java
+++ common/kadai-common/src/main/java/io/kadai/common/internal/configuration/DB.java
@@ -20,30 +20,53 @@ package io.kadai.common.internal.configuration;
 
 import io.kadai.common.api.exceptions.SystemException;
 import io.kadai.common.api.exceptions.UnsupportedDatabaseException;
+import io.kadai.common.internal.pagination.Db2PageDialect;
+import io.kadai.common.internal.pagination.H2PageDialect;
+import io.kadai.common.internal.pagination.PageDialect;
+import io.kadai.common.internal.pagination.PostgresPageDialect;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Arrays;
 
 /** Supported versions of databases. */
 public enum DB {
-  H2("H2", "h2", "/sql/h2/schema-detection-h2.sql", "/sql/h2/kadai-schema-h2.sql"),
-  DB2("DB2", "db2", "/sql/db2/schema-detection-db2.sql", "/sql/db2/kadai-schema-db2.sql"),
+  H2(
+      "H2",
+      "h2",
+      "/sql/h2/schema-detection-h2.sql",
+      "/sql/h2/kadai-schema-h2.sql",
+      new H2PageDialect()),
+  DB2(
+      "DB2",
+      "db2",
+      "/sql/db2/schema-detection-db2.sql",
+      "/sql/db2/kadai-schema-db2.sql",
+      new Db2PageDialect()),
   POSTGRES(
       "PostgreSQL",
       "postgres",
       "/sql/postgres/schema-detection-postgres.sql",
-      "/sql/postgres/kadai-schema-postgres.sql");
+      "/sql/postgres/kadai-schema-postgres.sql",
+      new PostgresPageDialect()),
+  ;
 
   public final String dbProductName;
   public final String dbProductId;
   public final String detectionScript;
   public final String schemaScript;
+  public final PageDialect dialect;
 
-  DB(String dbProductName, String dbProductId, String detectionScript, String schemaScript) {
+  DB(
+      String dbProductName,
+      String dbProductId,
+      String detectionScript,
+      String schemaScript,
+      PageDialect dialect) {
     this.dbProductName = dbProductName;
     this.dbProductId = dbProductId;
     this.detectionScript = detectionScript;
     this.schemaScript = schemaScript;
+    this.dialect = dialect;
   }
 
   public static DB getDB(String dbProductId) {
