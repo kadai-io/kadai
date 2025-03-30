a1eb28cb1d0f11a4e8fcd815deaa846644e0f906
Julian Bruder <94740838+bruderj15@users.noreply.github.com>
2025-03-30 14:11:11 +0200

    Closes #669 - Remove support for CDI & Wildfly-Example

--- ci/verify_docs_jar.sh
+++ ci/verify_docs_jar.sh
@@ -16,10 +16,8 @@ JAR_CONTENT=$(jar -tf "$JAR_FILE_LOCATION")
 set -x
 
 verifyDocs "$REL/../lib/kadai-core/target/reports/apidocs" "/static/docs/java/kadai-core"
-verifyDocs "$REL/../lib/kadai-cdi/target/reports/apidocs" "/static/docs/java/kadai-cdi"
 verifyDocs "$REL/../lib/kadai-spring/target/reports/apidocs" "/static/docs/java/kadai-spring"
 test -n "$(jar -tf "$JAR_FILE_LOCATION" | grep /static/docs/java/kadai-core/index.html)"
 test -n "$(jar -tf "$JAR_FILE_LOCATION" | grep /static/docs/java/kadai-spring/index.html)"
-test -n "$(jar -tf "$JAR_FILE_LOCATION" | grep /static/docs/java/kadai-cdi/io/kadai/common/internal/package-summary.html)"
 set +x
 echo "the jar file '$JAR_FILE_LOCATION' contains documentation"
