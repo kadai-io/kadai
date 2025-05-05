efa87651a18ae0b2caf51c98f83b2b47039fecfb
Julian Bruder <julian.bruder@outlook.com>
2025-05-05 11:31:57 +0200

    KAD-953: test production of `TaskClaimedReviewEvent`

--- history/kadai-simplehistory-provider/src/test/java/acceptance/events/task/CreateHistoryEventOnTaskClaimAccTest.java
+++ history/kadai-simplehistory-provider/src/test/java/acceptance/events/task/CreateHistoryEventOnTaskClaimAccTest.java
@@ -21,7 +21,6 @@ package acceptance.events.task;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import acceptance.AbstractAccTest;
-import io.kadai.common.internal.util.Pair;
 import io.kadai.common.test.security.JaasExtension;
 import io.kadai.common.test.security.WithAccessId;
 import io.kadai.simplehistory.impl.SimpleHistoryServiceImpl;
@@ -34,13 +33,12 @@ import io.kadai.task.api.TaskState;
 import io.kadai.task.api.models.Task;
 import java.time.Instant;
 import java.util.List;
-import java.util.stream.Stream;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.junit.jupiter.api.DynamicTest;
-import org.junit.jupiter.api.TestFactory;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.function.ThrowingConsumer;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @ExtendWith(JaasExtension.class)
 class CreateHistoryEventOnTaskClaimAccTest extends AbstractAccTest {
@@ -49,86 +47,136 @@ class CreateHistoryEventOnTaskClaimAccTest extends AbstractAccTest {
   private final SimpleHistoryServiceImpl historyService = getHistoryService();
 
   @WithAccessId(user = "admin")
-  @TestFactory
-  Stream<DynamicTest> should_CreateClaimedHistoryEvent_When_TaskIsClaimed() {
-    List<Pair<String, String>> list =
-        List.of(
-            Pair.of(
-                "With Attachment and secondary Object Reference",
-                "TKI:000000000000000000000000000000000053"),
-            Pair.of(
-                "Without Attachment and secondary Object References",
-                "TKI:000000000000000000000000000000000047"));
+  @ParameterizedTest
+  @ValueSource(
+      strings = {
+        "TKI:000000000000000000000000000000000053",
+        "TKI:000000000000000000000000000000000047"
+      })
+  void should_CreateClaimedHistoryEvent_When_TaskIsClaimedFromReady(String taskId)
+      throws Exception {
+    final Instant oldModified = taskService.getTask(taskId).getModified();
 
-    ThrowingConsumer<Pair<String, String>> test =
-        pair -> {
-          final Instant oldModified = taskService.getTask(pair.getRight()).getModified();
+    TaskHistoryQueryMapper taskHistoryQueryMapper = getHistoryQueryMapper();
 
-          TaskHistoryQueryMapper taskHistoryQueryMapper = getHistoryQueryMapper();
+    List<TaskHistoryEvent> events =
+        taskHistoryQueryMapper.queryHistoryEvents(
+            (TaskHistoryQueryImpl) historyService.createTaskHistoryQuery().taskIdIn(taskId));
 
-          List<TaskHistoryEvent> events =
-              taskHistoryQueryMapper.queryHistoryEvents(
-                  (TaskHistoryQueryImpl)
-                      historyService.createTaskHistoryQuery().taskIdIn(pair.getRight()));
+    assertThat(events).isEmpty();
 
-          assertThat(events).isEmpty();
+    assertThat(taskService.getTask(taskId).getState()).isEqualTo(TaskState.READY);
+    Task task = taskService.claim(taskId);
 
-          assertThat(taskService.getTask(pair.getRight()).getState()).isEqualTo(TaskState.READY);
-          Task task = taskService.claim(pair.getRight());
+    assertThat(task.getState()).isEqualTo(TaskState.CLAIMED);
 
-          assertThat(task.getState()).isEqualTo(TaskState.CLAIMED);
+    events =
+        taskHistoryQueryMapper.queryHistoryEvents(
+            (TaskHistoryQueryImpl) historyService.createTaskHistoryQuery().taskIdIn(taskId));
 
-          events =
-              taskHistoryQueryMapper.queryHistoryEvents(
-                  (TaskHistoryQueryImpl)
-                      historyService.createTaskHistoryQuery().taskIdIn(pair.getRight()));
+    TaskHistoryEvent event = events.get(0);
 
-          TaskHistoryEvent event = events.get(0);
+    assertThat(event.getEventType()).isEqualTo(TaskHistoryEventType.CLAIMED.getName());
 
-          assertThat(event.getEventType()).isEqualTo(TaskHistoryEventType.CLAIMED.getName());
+    event = historyService.getTaskHistoryEvent(event.getId());
 
-          event = historyService.getTaskHistoryEvent(event.getId());
+    assertThat(event.getDetails()).isNotNull();
 
-          assertThat(event.getDetails()).isNotNull();
+    JSONArray changes = new JSONObject(event.getDetails()).getJSONArray("changes");
 
-          JSONArray changes = new JSONObject(event.getDetails()).getJSONArray("changes");
+    JSONObject expectedClaimed =
+        new JSONObject()
+            .put("newValue", task.getModified().toString())
+            .put("fieldName", "claimed")
+            .put("oldValue", "");
+    JSONObject expectedModified =
+        new JSONObject()
+            .put("newValue", task.getModified().toString())
+            .put("fieldName", "modified")
+            .put("oldValue", oldModified.toString());
+    JSONObject expectedState =
+        new JSONObject()
+            .put("newValue", TaskState.CLAIMED.name())
+            .put("fieldName", "state")
+            .put("oldValue", TaskState.READY.name());
+    JSONObject expectedOwner =
+        new JSONObject().put("newValue", "admin").put("fieldName", "owner").put("oldValue", "");
+    JSONObject expectedIsRead =
+        new JSONObject().put("newValue", true).put("fieldName", "isRead").put("oldValue", false);
 
-          JSONObject expectedClaimed =
-              new JSONObject()
-                  .put("newValue", task.getModified().toString())
-                  .put("fieldName", "claimed")
-                  .put("oldValue", "");
-          JSONObject expectedModified =
-              new JSONObject()
-                  .put("newValue", task.getModified().toString())
-                  .put("fieldName", "modified")
-                  .put("oldValue", oldModified.toString());
-          JSONObject expectedState =
-              new JSONObject()
-                  .put("newValue", TaskState.CLAIMED.name())
-                  .put("fieldName", "state")
-                  .put("oldValue", TaskState.READY.name());
-          JSONObject expectedOwner =
-              new JSONObject()
-                  .put("newValue", "admin")
-                  .put("fieldName", "owner")
-                  .put("oldValue", "");
-          JSONObject expectedIsRead =
-              new JSONObject()
-                  .put("newValue", true)
-                  .put("fieldName", "isRead")
-                  .put("oldValue", false);
+    JSONArray expectedChanges =
+        new JSONArray()
+            .put(expectedClaimed)
+            .put(expectedModified)
+            .put(expectedState)
+            .put(expectedOwner)
+            .put(expectedIsRead);
 
-          JSONArray expectedChanges =
-              new JSONArray()
-                  .put(expectedClaimed)
-                  .put(expectedModified)
-                  .put(expectedState)
-                  .put(expectedOwner)
-                  .put(expectedIsRead);
+    assertThat(changes.similar(expectedChanges)).isTrue();
+  }
 
-          assertThat(changes.similar(expectedChanges)).isTrue();
-        };
-    return DynamicTest.stream(list.iterator(), Pair::getLeft, test);
+  @Test
+  @WithAccessId(user = "admin")
+  void should_CreateClaimedReviewHistoryEvent_When_TaskIsClaimedFromReadyForReview()
+      throws Exception {
+    final String taskId = "TKI:100000000000000000000000000000000025";
+    final Instant oldModified = taskService.getTask(taskId).getModified();
+
+    TaskHistoryQueryMapper taskHistoryQueryMapper = getHistoryQueryMapper();
+
+    List<TaskHistoryEvent> events =
+        taskHistoryQueryMapper.queryHistoryEvents(
+            (TaskHistoryQueryImpl) historyService.createTaskHistoryQuery().taskIdIn(taskId));
+
+    assertThat(events).isEmpty();
+
+    assertThat(taskService.getTask(taskId).getState()).isEqualTo(TaskState.READY_FOR_REVIEW);
+    Task task = taskService.claim(taskId);
+
+    assertThat(task.getState()).isEqualTo(TaskState.IN_REVIEW);
+
+    events =
+        taskHistoryQueryMapper.queryHistoryEvents(
+            (TaskHistoryQueryImpl) historyService.createTaskHistoryQuery().taskIdIn(taskId));
+
+    TaskHistoryEvent event = events.get(0);
+
+    assertThat(event.getEventType()).isEqualTo(TaskHistoryEventType.CLAIMED_REVIEW.getName());
+
+    event = historyService.getTaskHistoryEvent(event.getId());
+
+    assertThat(event.getDetails()).isNotNull();
+
+    JSONArray changes = new JSONObject(event.getDetails()).getJSONArray("changes");
+
+    JSONObject expectedClaimed =
+        new JSONObject()
+            .put("newValue", task.getModified().toString())
+            .put("fieldName", "claimed")
+            .put("oldValue", "");
+    JSONObject expectedModified =
+        new JSONObject()
+            .put("newValue", task.getModified().toString())
+            .put("fieldName", "modified")
+            .put("oldValue", oldModified.toString());
+    JSONObject expectedState =
+        new JSONObject()
+            .put("newValue", TaskState.IN_REVIEW.name())
+            .put("fieldName", "state")
+            .put("oldValue", TaskState.READY_FOR_REVIEW.name());
+    JSONObject expectedOwner =
+        new JSONObject().put("newValue", "admin").put("fieldName", "owner").put("oldValue", "");
+    JSONObject expectedIsRead =
+        new JSONObject().put("newValue", true).put("fieldName", "isRead").put("oldValue", false);
+
+    JSONArray expectedChanges =
+        new JSONArray()
+            .put(expectedClaimed)
+            .put(expectedModified)
+            .put(expectedState)
+            .put(expectedOwner)
+            .put(expectedIsRead);
+
+    assertThat(changes.similar(expectedChanges)).isTrue();
   }
 }
