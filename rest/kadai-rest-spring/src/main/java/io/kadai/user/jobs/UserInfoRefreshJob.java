aed7d69364d73dcab35e523d0f4e694a51a2e7c9
Julian Bruder <julian.bruder@outlook.com>
2025-07-14 11:57:13 +0200

    KAD-1207: log and skip error-prone users when refreshing from ldap

--- rest/kadai-rest-spring/src/main/java/io/kadai/user/jobs/UserInfoRefreshJob.java
+++ rest/kadai-rest-spring/src/main/java/io/kadai/user/jobs/UserInfoRefreshJob.java
@@ -68,21 +68,14 @@ public class UserInfoRefreshJob extends AbstractKadaiJob {
   @Override
   protected void execute() {
     LOGGER.info("Running job to refresh all user info");
-
-    LdapClient ldapClient =
-        ApplicationContextProvider.getApplicationContext().getBean("ldapClient", LdapClient.class);
-
     try {
-
+      final LdapClient ldapClient =
+          ApplicationContextProvider.getApplicationContext()
+              .getBean("ldapClient", LdapClient.class);
       List<User> users = ldapClient.searchUsersInUserRole();
-      List<User> usersAfterProcessing =
-          users.stream().map(refreshUserPostprocessorManager::processUserAfterRefresh).toList();
-      addExistingConfigurationDataToUsers(usersAfterProcessing);
       clearExistingUsersAndGroupsAndPermissions();
-      insertNewUsers(usersAfterProcessing);
-
+      users.forEach(this::executeUser);
       LOGGER.info("Job to refresh all user info has finished.");
-
     } catch (Exception e) {
       throw new SystemException("Error while processing UserRefreshJob.", e);
     }
@@ -101,55 +94,56 @@ public class UserInfoRefreshJob extends AbstractKadaiJob {
     }
   }
 
-  private void insertNewUsers(List<User> users) {
-
-    users.forEach(
-        user -> {
-          try {
-            if (LOGGER.isDebugEnabled()) {
-              LOGGER.debug("Trying to insert user {}", user);
-            }
-            kadaiEngineImpl.getUserService().createUser(user);
-            if (LOGGER.isDebugEnabled()) {
-              LOGGER.debug("Successfully inserted user {}", user);
-            }
-          } catch (InvalidArgumentException
-              | NotAuthorizedException
-              | UserAlreadyExistException e) {
-            throw new SystemException("Caught Exception while trying to insert new User", e);
-          }
-        });
+  private void executeUser(User user) {
+    try {
+      final User userAfterProcessing =
+          refreshUserPostprocessorManager.processUserAfterRefresh(user);
+      addExistingConfigurationDataToUser(userAfterProcessing);
+      insertNewUser(userAfterProcessing);
+    } catch (Exception e) {
+      LOGGER.error("Failed refreshing user {}", user, e);
+    }
   }
 
-  private void addExistingConfigurationDataToUsers(List<User> users) {
+  private void insertNewUser(User user) {
+    try {
+      if (LOGGER.isDebugEnabled()) {
+        LOGGER.debug("Trying to insert user {}", user);
+      }
+      kadaiEngineImpl.getUserService().createUser(user);
+      if (LOGGER.isDebugEnabled()) {
+        LOGGER.debug("Successfully inserted user {}", user);
+      }
+    } catch (InvalidArgumentException | NotAuthorizedException | UserAlreadyExistException e) {
+      throw new SystemException("Caught Exception while trying to insert new User", e);
+    }
+  }
 
-    users.forEach(
-        user -> {
-          try {
+  private void addExistingConfigurationDataToUser(User user) {
+    try {
 
-            String userData = kadaiEngineImpl.getUserService().getUser(user.getId()).getData();
-            if (userData != null) {
-              if (LOGGER.isDebugEnabled()) {
-                LOGGER.debug("Trying to set userData {} for user {}", userData, user);
-              }
-              user.setData(userData);
-              if (LOGGER.isDebugEnabled()) {
-                LOGGER.debug("Successfully set userData {} for user {}", userData, user);
-              }
-            }
-          } catch (UserNotFoundException e) {
-            if (LOGGER.isDebugEnabled()) {
-              LOGGER.debug(
-                  String.format(
-                      "Failed to fetch configuration data for User "
-                          + "with ID '%s' because it doesn't exist",
-                      user.getId()));
-            }
-          } catch (InvalidArgumentException e) {
-            if (LOGGER.isDebugEnabled()) {
-              LOGGER.debug("Failed to fetch configuration data because userId was NULL or empty");
-            }
-          }
-        });
+      String userData = kadaiEngineImpl.getUserService().getUser(user.getId()).getData();
+      if (userData != null) {
+        if (LOGGER.isDebugEnabled()) {
+          LOGGER.debug("Trying to set userData {} for user {}", userData, user);
+        }
+        user.setData(userData);
+        if (LOGGER.isDebugEnabled()) {
+          LOGGER.debug("Successfully set userData {} for user {}", userData, user);
+        }
+      }
+    } catch (UserNotFoundException e) {
+      if (LOGGER.isDebugEnabled()) {
+        LOGGER.debug(
+            String.format(
+                "Failed to fetch configuration data for User "
+                    + "with ID '%s' because it doesn't exist",
+                user.getId()));
+      }
+    } catch (InvalidArgumentException e) {
+      if (LOGGER.isDebugEnabled()) {
+        LOGGER.debug("Failed to fetch configuration data because userId was NULL or empty");
+      }
+    }
   }
 }
