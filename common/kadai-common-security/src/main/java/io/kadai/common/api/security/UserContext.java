678dbcc2a497974a8a749d6385012a75d19fc085
Julian Bruder <julian.bruder@outlook.com>
2025-07-23 12:58:08 +0200

    KAD-1208: refine getter

--- common/kadai-common-security/src/main/java/io/kadai/common/api/security/UserContext.java
+++ common/kadai-common-security/src/main/java/io/kadai/common/api/security/UserContext.java
@@ -18,8 +18,6 @@
 
 package io.kadai.common.api.security;
 
-import java.util.Optional;
-
 public interface UserContext {
 
   default String getUserId() {
@@ -28,5 +26,5 @@ public interface UserContext {
 
   String getPuppet();
 
-  Optional<String> getPuppeteer();
+  String getPuppeteer();
 }
