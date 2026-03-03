9b398e0530ff0d4bcf6785e3d19304b8eb42dc56
Julian Bruder <julian.bruder@outlook.com>
2026-03-03 13:45:43 +0100

    task#1740: add placeholder workaround class for https://github.com/springdoc/springdoc-openapi/issues/3238

---
+++ rest/kadai-rest-spring-example-boot/src/main/java/org/springframework/boot/hateoas/autoconfigure/HateoasProperties.java
@@ -0,0 +1,5 @@
+package org.springframework.boot.hateoas.autoconfigure;
+
+// Workaround springdoc-openapi relies on removed class
+// See https://github.com/springdoc/springdoc-openapi/issues/3238
+public class HateoasProperties {}
