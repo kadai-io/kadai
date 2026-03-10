38e144c149e2f9c7cdb3376dc362934d15813992
Julian Bruder <julian.bruder@outlook.com>
2026-03-10 10:43:19 +0100

    feat#1757: add `TaskIdPagedRepresentationModel`

---
+++ rest/kadai-rest-spring/src/main/java/io/kadai/task/rest/models/TaskIdPagedRepresentationModel.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright [2026] [envite consulting GmbH]
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ *
+ */
+
+package io.kadai.task.rest.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.kadai.common.rest.models.PageMetadata;
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.beans.ConstructorProperties;
+import java.util.Collection;
+import org.springframework.hateoas.RepresentationModel;
+
+/**
+ * Representation model for a paged list of Task IDs. This is a lightweight alternative to {@link
+ * TaskSummaryPagedRepresentationModel} that only returns the Task IDs instead of full Task
+ * summaries.
+ */
+public class TaskIdPagedRepresentationModel
+    extends RepresentationModel<TaskIdPagedRepresentationModel> {
+
+  @Schema(name = "taskIds", description = "The task IDs matching the query.")
+  @JsonProperty("taskIds")
+  private final Collection<String> taskIds;
+
+  @Schema(name = "page", description = "The page meta data for a paged request.")
+  @JsonProperty("page")
+  private final PageMetadata pageMetadata;
+
+  @ConstructorProperties({"taskIds", "page"})
+  public TaskIdPagedRepresentationModel(Collection<String> taskIds, PageMetadata pageMetadata) {
+    this.taskIds = taskIds;
+    this.pageMetadata = pageMetadata;
+  }
+
+  public Collection<String> getTaskIds() {
+    return taskIds;
+  }
+
+  public PageMetadata getPageMetadata() {
+    return pageMetadata;
+  }
+}
