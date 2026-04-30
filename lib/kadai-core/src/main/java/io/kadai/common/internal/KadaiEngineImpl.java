5e52b0547e535f590b26f9640998ca8682f0695d
Julian Bruder <julian.bruder@outlook.com>
2026-04-30 09:54:58 +0200

    Closes #1883 - Reduced payloads in TaskCleanupJob (#1884) (#1892)
    * task#1883: TaskCleanupJob to read only required data
    
    * task#1883: rename new query method to fit actual query better
    
    * task#1883: move `TaskCleanupSummary` into `...jobs.models`
    
    * task#1883: single aggregate query instead of n bpid roundtrips
    
    * task#1883: add migration scripts for recommended indexes
    
    * task#1883: fix h2 quirks
    
    * task#1883: add more explicit tests for `TaskCleanupJob`
    
    * task#1883: add on v12 migration script
    
    (cherry picked from commit 9e1e14d718b507655e6fe7aa74c8f7085bc55df0)

--- lib/kadai-core/src/main/java/io/kadai/common/internal/KadaiEngineImpl.java
+++ lib/kadai-core/src/main/java/io/kadai/common/internal/KadaiEngineImpl.java
@@ -295,7 +295,7 @@ public class KadaiEngineImpl implements KadaiEngine {
 
   // This should be part of the InternalKadaiEngine. Unfortunately the jobs don't have access to
   // that engine.
-  // Therefore, this getter exits and will be removed as soon as our jobs will be refactored.
+  // Therefore, this getter exists and will be removed as soon as our jobs will be refactored.
   public PriorityServiceManager getPriorityServiceManager() {
     return priorityServiceManager;
   }
---
+++ lib/kadai-core/src/main/java/io/kadai/task/internal/jobs/models/TaskCleanupSummary.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright [2024] [envite consulting GmbH]
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ *
+ */
+
+package io.kadai.task.internal.jobs.models;
+
+import java.util.Objects;
+
+/**
+ * A lightweight summary used by the TaskCleanupJob. Contains only the fields needed for cleanup
+ * logic: task id and parent business process id.
+ */
+public class TaskCleanupSummary {
+
+  private String taskId;
+  private String parentBusinessProcessId;
+
+  public TaskCleanupSummary() {}
+
+  public String getTaskId() {
+    return taskId;
+  }
+
+  public void setTaskId(String taskId) {
+    this.taskId = taskId;
+  }
+
+  public String getParentBusinessProcessId() {
+    return parentBusinessProcessId;
+  }
+
+  public void setParentBusinessProcessId(String parentBusinessProcessId) {
+    this.parentBusinessProcessId = parentBusinessProcessId;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(taskId, parentBusinessProcessId);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (!(obj instanceof TaskCleanupSummary)) {
+      return false;
+    }
+    TaskCleanupSummary other = (TaskCleanupSummary) obj;
+    return Objects.equals(taskId, other.taskId)
+        && Objects.equals(parentBusinessProcessId, other.parentBusinessProcessId);
+  }
+
+  @Override
+  public String toString() {
+    return "TaskCleanupSummary [taskId="
+        + taskId
+        + ", parentBusinessProcessId="
+        + parentBusinessProcessId
+        + "]";
+  }
+}
