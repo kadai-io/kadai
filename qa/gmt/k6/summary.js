f519cd23fcdb1e7267872d9068228961e3f8d0a2
David Kopp <git@davidkopp.de>
2026-05-15 10:23:12 +0200

    Use GMT supported units (us instead of ms)

--- qa/gmt/k6/summary.js
+++ qa/gmt/k6/summary.js
@@ -1,16 +1,20 @@
 import { textSummary } from "https://jslib.k6.io/k6-summary/0.1.0/index.js";
 
+function millisecondsToMicroseconds(value) {
+  return Math.round(value * 1000);
+}
+
 export function handleSummary(data) {
-    const timestamp = Date.now() * 1000; // milliseconds → microseconds
+    const timestamp = millisecondsToMicroseconds(Date.now())
 
     const duration = data.metrics.http_req_duration?.values ?? {};
     const throughput = data.metrics.http_reqs?.values?.rate ?? 0;
 
     const linesWithGmtCustomMetrics = [
-        `${timestamp} http_req_duration_avg ${Math.round(duration.avg)}`,
-        `${timestamp} http_req_duration_min ${Math.round(duration.min)}`,
-        `${timestamp} http_req_duration_max ${Math.round(duration.max)}`,
-        `${timestamp} http_req_duration_p95 ${Math.round(duration["p(95)"])}`,
+        `${timestamp} http_req_duration_avg ${millisecondsToMicroseconds(duration.avg)}`,
+        `${timestamp} http_req_duration_min ${millisecondsToMicroseconds(duration.min)}`,
+        `${timestamp} http_req_duration_max ${millisecondsToMicroseconds(duration.max)}`,
+        `${timestamp} http_req_duration_p95 ${millisecondsToMicroseconds(duration["p(95)"])}`,
         `${timestamp} http_reqs_per_second  ${Math.round(throughput)}`,
     ].join("\n");
 
