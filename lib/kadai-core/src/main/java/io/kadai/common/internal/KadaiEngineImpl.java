6c4a539f3327c86ba9c30660436a4a9456dca1e7
Julian Bruder <julian.bruder@outlook.com>
2026-06-15 10:42:21 +0200

    hide mapper access in service-impl

--- lib/kadai-core/src/main/java/io/kadai/common/internal/KadaiEngineImpl.java
+++ lib/kadai-core/src/main/java/io/kadai/common/internal/KadaiEngineImpl.java
@@ -300,20 +300,6 @@ public class KadaiEngineImpl implements KadaiEngine {
     return priorityServiceManager;
   }
 
-  // This should be part of the InternalKadaiEngine. Unfortunately the jobs don't have access to
-  // that engine.
-  // Therefore, this getter exists and will be removed as soon as our jobs will be refactored.
-  public TaskMapper getTaskMapper() {
-    return sessionManager.getMapper(TaskMapper.class);
-  }
-
-  // This should be part of the InternalKadaiEngine. Unfortunately the jobs don't have access to
-  // that engine.
-  // Therefore, this delegate exists and will be removed as soon as our jobs will be refactored.
-  public <T> T executeInDatabaseConnection(Supplier<T> supplier) {
-    return internalKadaiEngineImpl.executeInDatabaseConnection(supplier);
-  }
-
   @Override
   public JobService getJobService() {
     return new JobServiceImpl(internalKadaiEngineImpl, sessionManager.getMapper(JobMapper.class));
