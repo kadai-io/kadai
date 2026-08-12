59d5433d6afb3edfe067a00d1761590bce735a7e
Julian Bruder <94740838+bruderj15@users.noreply.github.com>
2026-08-12 17:11:06 +0200

    Closes #1898 - Add `kadai-gmt` application (#2116)
    * add `kadai-gmt`
    
    * fix payload
    
    * fix payload pt2

---
+++ rest/kadai-gmt/src/main/java/io/kadai/gmt/TaskTestDataController.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright [2026] [envite consulting GmbH]
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ *
+ */
+
+package io.kadai.gmt;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.kadai.common.api.KadaiEngine;
+import io.kadai.testapi.generator.GenerationSummary;
+import io.kadai.testapi.generator.TaskTestDataGenerator;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/** Creates the data set used by Green Metrics Tool measurements. */
+@RestController
+public class TaskTestDataController {
+
+  private static final int BATCH_SIZE = 1_000;
+
+  private final KadaiEngine kadaiEngine;
+
+  public TaskTestDataController(KadaiEngine kadaiEngine) {
+    this.kadaiEngine = kadaiEngine;
+  }
+
+  /**
+   * Creates compatible classifications and workbaskets as needed, then persists the requested
+   * tasks. The response is returned only after the database transaction has committed.
+   *
+   * @param request the requested number of tasks
+   * @return counts of the persisted data and supporting fixtures
+   */
+  @PostMapping(
+      path = "/api/v1/gmt/tasks",
+      consumes = MediaType.APPLICATION_JSON_VALUE,
+      produces = MediaType.APPLICATION_JSON_VALUE)
+  public GenerationSummary createTasks(@Valid @RequestBody TaskGenerationRequest request) {
+    return TaskTestDataGenerator.from(kadaiEngine).persist(request.taskCount(), BATCH_SIZE);
+  }
+
+  /**
+   * Request body for synchronous task test-data generation.
+   *
+   * @param taskCount number of tasks to create
+   */
+  public record TaskGenerationRequest(
+      @JsonProperty("taskCount") @NotNull @Min(0) Long taskCount) {
+
+    @JsonCreator
+    public TaskGenerationRequest {}
+  }
+}
---
+++ rest/kadai-gmt/src/main/java/io/kadai/gmt/security/BootWebSecurityConfigurer.java
@@ -0,0 +1,210 @@
+/*
+ * Copyright [2026] [envite consulting GmbH]
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ *
+ */
+
+package io.kadai.gmt.security;
+
+import static io.kadai.common.rest.ldap.LdapConfiguration.KADAI_LDAP_CONTEXT_SOURCE;
+
+import io.kadai.common.rest.SpringSecurityToJaasFilter;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.ldap.core.ContextSource;
+import org.springframework.ldap.core.support.BaseLdapPathContextSource;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.annotation.web.configurers.RequestCacheConfigurer;
+import org.springframework.security.config.ldap.LdapPasswordComparisonAuthenticationManagerFactory;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
+import org.springframework.security.ldap.userdetails.DefaultLdapAuthoritiesPopulator;
+import org.springframework.security.ldap.userdetails.LdapAuthoritiesPopulator;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.jaasapi.JaasApiIntegrationFilter;
+import org.springframework.security.web.servlet.util.matcher.PathPatternRequestMatcher;
+
+/** Default basic configuration for kadai web example. */
+@Configuration
+public class BootWebSecurityConfigurer {
+
+  private final String ldapServerUrl;
+  private final String ldapBaseDn;
+  private final String ldapUserDnPatterns;
+  private final String ldapGroupSearchBase;
+  private final String ldapGroupSearchFilter;
+  private final String ldapPermissionSearchBase;
+  private final String ldapPermissionSearchFilter;
+
+  private final boolean devMode;
+  private final boolean enableCsrf;
+
+  public BootWebSecurityConfigurer(
+      @Value("${kadai.ldap.serverUrl:ldap://localhost:10389}") String ldapServerUrl,
+      @Value("${kadai.ldap.baseDn:OU=Test,O=KADAI}") String ldapBaseDn,
+      @Value("${kadai.ldap.userDnPatterns:uid={0},cn=users}") String ldapUserDnPatterns,
+      @Value("${kadai.ldap.groupSearchBase:cn=groups}") String ldapGroupSearchBase,
+      @Value("${kadai.ldap.groupSearchFilter:uniqueMember={0}}") String ldapGroupSearchFilter,
+      @Value("${kadai.ldap.permissionSearchBase:cn=permissions}") String ldapPermissionSearchBase,
+      @Value("${kadai.ldap.permissionSearchFilter:uniqueMember={0}}")
+          String ldapPermissionSearchFilter,
+      @Value("${enableCsrf:false}") boolean enableCsrf,
+      @Value("${devMode:false}") boolean devMode) {
+    this.enableCsrf = enableCsrf;
+    this.ldapServerUrl = ldapServerUrl;
+    this.ldapBaseDn = ldapBaseDn;
+    this.ldapGroupSearchBase = ldapGroupSearchBase;
+    this.ldapGroupSearchFilter = ldapGroupSearchFilter;
+    this.ldapPermissionSearchBase = ldapPermissionSearchBase;
+    this.ldapPermissionSearchFilter = ldapPermissionSearchFilter;
+    this.ldapUserDnPatterns = ldapUserDnPatterns;
+    this.devMode = devMode;
+  }
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests(
+            authorizeHttpRequests ->
+                authorizeHttpRequests
+                    .requestMatchers("/css/**", "/img/**")
+                    .permitAll()
+                    .requestMatchers(HttpMethod.GET, "/docs/**")
+                    .permitAll()
+                    .requestMatchers(
+                        HttpMethod.GET,
+                        "/api-docs",
+                        "/api-docs/**",
+                        "/swagger-ui",
+                        "/swagger-ui/**")
+                    .permitAll())
+        .cors(Customizer.withDefaults())
+        .addFilter(jaasApiIntegrationFilter())
+        .addFilterAfter(new SpringSecurityToJaasFilter(), JaasApiIntegrationFilter.class);
+
+    if (enableCsrf) {
+      CookieCsrfTokenRepository csrfTokenRepository = CookieCsrfTokenRepository.withHttpOnlyFalse();
+      csrfTokenRepository.setCookiePath("/");
+      http.csrf(
+              csrf ->
+                  csrf.csrfTokenRepository(csrfTokenRepository)
+                      .csrfTokenRequestHandler(new SpaCsrfTokenRequestHandler()))
+          .addFilterAfter(new CsrfCookieFilter(), SpringSecurityToJaasFilter.class);
+    } else {
+      http.csrf(AbstractHttpConfigurer::disable).httpBasic(Customizer.withDefaults());
+    }
+
+    if (devMode) {
+      http.headers(
+              headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+          .authorizeHttpRequests(
+              authorizeHttpRequests ->
+                  authorizeHttpRequests
+                      .requestMatchers("/h2-console/**")
+                      .permitAll()
+                      .anyRequest()
+                      .fullyAuthenticated())
+          .logout(logout -> logout.logoutSuccessUrl("http://localhost:4200/#").permitAll());
+    } else {
+      addLoginPageConfiguration(http);
+    }
+    http.requestCache(RequestCacheConfigurer::disable);
+    return http.build();
+  }
+
+  @Bean
+  public LdapAuthoritiesPopulator authoritiesPopulator(
+      @Qualifier(KADAI_LDAP_CONTEXT_SOURCE) ContextSource contextSource) {
+    Function<Map<String, List<String>>, GrantedAuthority> authorityMapper =
+        recordVar -> new SimpleGrantedAuthority(recordVar.get("spring.security.ldap.dn").get(0));
+
+    DefaultLdapAuthoritiesPopulator populator =
+        new DefaultLdapAuthoritiesPopulator(contextSource, ldapGroupSearchBase);
+    populator.setGroupSearchFilter(ldapGroupSearchFilter);
+    populator.setSearchSubtree(true);
+    populator.setRolePrefix("");
+    populator.setAuthorityMapper(authorityMapper);
+    return populator;
+  }
+
+  @Bean(name = KADAI_LDAP_CONTEXT_SOURCE)
+  public BaseLdapPathContextSource ldapContextSource() {
+    return new DefaultSpringSecurityContextSource(ldapServerUrl + "/" + ldapBaseDn);
+  }
+
+  @Bean
+  public GrantedAuthoritiesMapper grantedAuthoritiesMapper() {
+    SimpleAuthorityMapper grantedAuthoritiesMapper = new SimpleAuthorityMapper();
+    grantedAuthoritiesMapper.setPrefix("");
+    return grantedAuthoritiesMapper;
+  }
+
+  protected void addLoginPageConfiguration(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests(
+            authorizeHttpRequests -> authorizeHttpRequests.anyRequest().fullyAuthenticated())
+        .formLogin(
+            formLogin ->
+                formLogin
+                    .loginPage("/login")
+                    .failureUrl("/login?error")
+                    .defaultSuccessUrl("/index.html")
+                    .permitAll())
+        .logout(
+            logout ->
+                logout
+                    .invalidateHttpSession(true)
+                    .clearAuthentication(true)
+                    .logoutRequestMatcher(
+                        PathPatternRequestMatcher.withDefaults().matcher("/logout"))
+                    .logoutSuccessUrl("/login?logout")
+                    .deleteCookies("JSESSIONID")
+                    .permitAll());
+  }
+
+  protected JaasApiIntegrationFilter jaasApiIntegrationFilter() {
+    JaasApiIntegrationFilter filter = new JaasApiIntegrationFilter();
+    filter.setCreateEmptySubject(true);
+    return filter;
+  }
+
+  @Bean
+  AuthenticationManager ldapAuthenticationManager(
+      @Qualifier(KADAI_LDAP_CONTEXT_SOURCE) BaseLdapPathContextSource contextSource,
+      LdapAuthoritiesPopulator authorities) {
+    @SuppressWarnings("deprecation")
+    LdapPasswordComparisonAuthenticationManagerFactory factory =
+        new LdapPasswordComparisonAuthenticationManagerFactory(
+            contextSource, NoOpPasswordEncoder.getInstance());
+    factory.setUserDnPatterns(ldapUserDnPatterns);
+    factory.setLdapAuthoritiesPopulator(authorities);
+    factory.setAuthoritiesMapper(grantedAuthoritiesMapper());
+    factory.setPasswordAttribute("userPassword");
+    return factory.createAuthenticationManager();
+  }
+}
